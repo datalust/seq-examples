d6f77799821203cf193e9dd75c5bde8510b38391
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-01-18 15:27:16 +1000

    Pare down the C# example a little

--- opentelemetry-csharp-sdk-to-seq/Example.WeatherService/Program.cs
+++ opentelemetry-csharp-sdk-to-seq/Example.WeatherService/Program.cs
@@ -3,74 +3,60 @@ using OpenTelemetry.Exporter;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+// ReSharper disable ExplicitCallerInfoArgument
 
-ActivitySource ExampleWeatherServiceActivitySource = new("Example Weather Service");
+var exampleActivitySource = new ActivitySource("Example.WeatherService");
 
-try
-{
-    var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-    builder.Services.AddOpenTelemetry()
-        .ConfigureResource(resource => resource.AddService(serviceName: builder.Environment.ApplicationName))
-        .WithTracing(tracing =>
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName: builder.Environment.ApplicationName))
+    .WithTracing(tracing =>
+    {
+        tracing.AddSource(exampleActivitySource.Name);
+        tracing.AddAspNetCoreInstrumentation();
+        tracing.AddConsoleExporter();
+        tracing.AddOtlpExporter(opt =>
         {
-            tracing.AddSource("Example Weather Service");
-            tracing.AddAspNetCoreInstrumentation();
-            tracing.AddConsoleExporter();
-            tracing.AddOtlpExporter(opt =>
-            {
-                opt.Endpoint = new Uri("http://localhost:5341/ingest/otlp/v1/traces");
-                opt.Protocol = OtlpExportProtocol.HttpProtobuf;
-                //opt.Headers = "X-Seq-ApiKey=abcde12345";
-            });
-        });
-
-    builder.Services.AddLogging(logging => logging.AddOpenTelemetry(openTelemetryLoggerOptions =>  
-    {  
-        openTelemetryLoggerOptions.SetResourceBuilder(  
-            ResourceBuilder.CreateEmpty()
-                .AddService(serviceName: builder.Environment.ApplicationName)  
-                .AddAttributes(new Dictionary<string, object>  
-                {
-                    // Add any desired resource attributes here
-                    ["deployment.environment"] = "development"  
-                }));
-  
-        // Some important options to improve data quality
-        openTelemetryLoggerOptions.IncludeScopes = true;
-        openTelemetryLoggerOptions.IncludeFormattedMessage = true;
-
-        openTelemetryLoggerOptions.AddConsoleExporter();
-        openTelemetryLoggerOptions.AddOtlpExporter(opt =>
-        {
-            opt.Endpoint = new Uri("http://localhost:5341/ingest/otlp/v1/logs");
+            opt.Endpoint = new Uri("http://localhost:5341/ingest/otlp/v1/traces");
             opt.Protocol = OtlpExportProtocol.HttpProtobuf;
             //opt.Headers = "X-Seq-ApiKey=abcde12345";
         });
-    }));
-
-    var app = builder.Build();
-    app.Logger.LogInformation("Starting {App}", builder.Environment.ApplicationName);
-
-    var forecastByPostcode = Directory.GetFiles("./data")
-        .ToDictionary(f => Path.GetFileNameWithoutExtension(f)!, f => File.ReadAllText(f).Trim());
-    
-    app.MapGet("/{postcode}", (string postcode) =>
-    {
-        using var activity = ExampleWeatherServiceActivitySource.StartActivity("Look up forecast for postcode {Postcode}");
-        activity?.SetTag("Postcode", postcode);
-        var forecast = forecastByPostcode[postcode];
-        activity?.SetTag("Forecast", forecast);
-        
-        return forecast;
     });
 
-    app.Run();
+builder.Services.AddLogging(logging => logging.AddOpenTelemetry(openTelemetryLoggerOptions =>  
+{  
+    openTelemetryLoggerOptions.SetResourceBuilder(  
+        ResourceBuilder.CreateEmpty()
+            .AddService(serviceName: builder.Environment.ApplicationName));
 
-    return 0;
-}
-catch (Exception)
+    // Some important options to improve data quality
+    openTelemetryLoggerOptions.IncludeScopes = true;
+
+    openTelemetryLoggerOptions.AddConsoleExporter();
+    openTelemetryLoggerOptions.AddOtlpExporter(opt =>
+    {
+        opt.Endpoint = new Uri("http://localhost:5341/ingest/otlp/v1/logs");
+        opt.Protocol = OtlpExportProtocol.HttpProtobuf;
+        //opt.Headers = "X-Seq-ApiKey=abcde12345";
+    });
+}));
+
+var app = builder.Build();
+app.Logger.LogInformation("Starting {App}", builder.Environment.ApplicationName);
+
+var forecastByPostcode = Directory.GetFiles("./data")
+    .ToDictionary(f => Path.GetFileNameWithoutExtension(f)!, f => File.ReadAllText(f).Trim());
+
+app.MapGet("/{postcode}", (string postcode) =>
 {
-    return 1;
-}
+    using var activity = exampleActivitySource.StartActivity("Look up forecast for postcode {Postcode}");
+    activity?.SetTag("Postcode", postcode);
+    
+    var forecast = forecastByPostcode[postcode];
+    activity?.SetTag("Forecast", forecast);
+    
+    return forecast;
+});
 
+app.Run();
